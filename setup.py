604c29a37ac76e9bc686d75733e41abbb17df143
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-05-21 13:04:37 +0200

    Modified setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name="rli_template",  # Required
+    name="WAM_server_api",  # Required
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
     #
@@ -66,7 +66,7 @@ setup(
     #
     # This field corresponds to the "Home-Page" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url="https://github.com/rl-institut/rli_template",  # Optional
+    url="https://github.com/rl-institut/WAM_server_api_template",  # Optional
     # This should be your name or the name of the organization which owns the
     # project.
     author="Reiner Lemoine Institut",  # Optional
@@ -173,7 +173,7 @@ setup(
     # maintainers, and where to support the project financially. The key is
     # what's used to render the link text on PyPI.
     project_urls={  # Optional
-        "Bug Reports": "https://github.com/rl-institut/rli_template/issues",
+        "Bug Reports": "https://github.com/rl-institut/WAM_server_api_template/issues",
         "Source": "https://github.com/rl-institut/rli_template",
     },
 )
